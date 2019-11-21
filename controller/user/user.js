f44bc37fe3d75506c3d94d62a3ba0b923adce8a4
Nthng <okkapong_not@hotmail.com>
2019-11-21 13:15:52 +0700

    [update]split config folder

--- controller/user/user.js
+++ controller/user/user.js
@@ -1,63 +1,66 @@
-const user = require('../../models/user')
-const profile = require('../../models/profile')
-const jwt = require('jsonwebtoken')
-
+const user = require("../../models/user");
+const profile = require("../../models/profile");
+const jwt = require("jsonwebtoken");
 
 module.exports = (req, res) => {
   user
     .findOne()
     .populate({
-      path: 'profile',
-      select: 'firstName'
+      path: "profile",
+      select: "firstName"
     })
-    .select('username')
+    .select("username")
     .exec((err, resp) => {
       if (err) {
-        console.log('err', err)
-        res.send(err)
+        console.log("err", err);
+        res.send(err);
+      } else {
+        console.log("res", resp);
+        res.send(resp);
       }
-      else {
-        console.log('res', resp)
-        res.send(resp)
-      }
-    })
-}
+    });
+};
 
 module.exports.signIn = async (req, res) => {
-  let data = await user.findOne({ username: req.body.username, password: req.body.password })
-  jwt.sign({ data }, 'Fri13th', { expiresIn: '1440s' }, (err, token) => {
+  let data = await user.findOne({
+    username: req.body.username,
+    password: req.body.password
+  });
+  jwt.sign({ data }, "Fri13th", { expiresIn: "1440s" }, (err, token) => {
     // console.log('token :', token);
     if (data) {
-      res.send({status: { code: 0, message: 'Login Successful', token: token}})
+      res.send({
+        status: { code: 1, message: "Login Successful", token: token }
+      });
     } else {
-      res.send({ status: { code: 1, message: 'Username or Password is Wrong' } })
+      res.send({
+        status: { code: 0, message: "Username or Password is Wrong" }
+      });
     }
-  })
-}
-
+  });
+};
 
 module.exports.signUp = (req, res) => {
   let Profile = new profile({
     username: req.body.username,
     password: req.body.password,
-    firstName: '',
-    lastName: '',
-    age: ''
-  })
+    firstName: "",
+    lastName: "",
+    age: ""
+  });
   let User = new user({
     username: req.body.username,
     password: req.body.password,
     profile: Profile.id
-  })
+  });
 
-  Profile.save()
-  User.save()
-    // .then(res.status(200).send('saved!!!'))
-    jwt.sign({ User }, 'Fri13th', { expiresIn: '1440s' }, (err, token) => {
-      if (err) {
-        res.send({status : { code : 1  , message : 'Failed'}})
-      }
-      res.send({status : { code : 0  , message : 'Successful' , token : token}})
-    })
-}
-
+  Profile.save();
+  User.save();
+  // .then(res.status(200).send('saved!!!'))
+  jwt.sign({ User }, "Fri13th", { expiresIn: "1440s" }, (err, token) => {
+    if (err) {
+      res.send({ status: { code: 0, message: "Failed" } });
+    }
+    res.send({ status: { code: 1, message: "Successful", token: token } });
+  });
+};
